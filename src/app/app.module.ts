b8e6359bf610b5edb4d624696a0c3cea1e44ba6d
Szymon Bakunowicz <szymon.bakunowicz@gmail.com>
2016-12-09 23:14:00 +0100

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes }   from '@angular/router';
 
+import {LoginComponent} from './login/login.component';
 import { AppComponent } from './app.component';
+import {RegisterComponent} from './register/register.component';
+import { routing } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
