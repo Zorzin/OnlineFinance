6761677244a57b0f6b64bb16b26b0781a57143bd
Szymon Bakunowicz <szymon.bakunowicz@gmail.com>
2016-12-11 16:55:36 +0100

    Adding receipt init

--- src/app/receipt/createreceipt.component.ts
+++ src/app/receipt/createreceipt.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
 import { User } from '../user';
 @Component({
   selector: 'createreceipt',
@@ -8,4 +9,28 @@ import { User } from '../user';
 
 export class CreateReciptComponent{
 
+  add = false;
+  create = true;
+  
+  reciptForm: FormGroup;
+  constructor(private formBuilder:FormBuilder){}
+  
+  ngOnInit(){
+    this.reciptForm = this.formBuilder.group({
+      shop: ['',Validators.required],
+      name: ['',Validators.required]
+    });  
+  }
+  
+  recipt(model,isValid:boolean){
+    if(isValid)
+    {
+      console.log(model);
+    }
+  }
+
+  Create(){
+    this.add=true;
+    this.create = false;
+  }
 }
